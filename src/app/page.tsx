8ef248763e57d1f865e69f1b96f3af1415d89f11
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2024-02-01 09:54:58 +0200

    configured route.ts file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import { Button } from "@nextui-org/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
-      <Button>Test</Button>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {session?.user ? <div>Signed in</div> : <div>Signed Out</div>}
     </div>
   );
 }
